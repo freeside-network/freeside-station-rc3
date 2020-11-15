e6a52f37ca51496f6415c7ac52d66fd664d74151
Martin Pugh <pugh@s3kr.it>
2020-11-15 21:40:01 +0100

    add beach

---
+++ tilesets/plain_door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="plain_door" tilewidth="32" tileheight="32" tilecount="30" columns="6">
+ <image source="../ss13-icons/obj/doors/airlocks/station/public.png" width="192" height="160"/>
+</tileset>
