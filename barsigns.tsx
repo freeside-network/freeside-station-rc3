57936dced677318218d3c59807daa5e43b078ea1
Martin Pugh <pugh@s3kr.it>
2020-11-15 18:56:52 +0100

    add a new room!

---
+++ barsigns.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="barsigns" tilewidth="32" tileheight="32" tilecount="812" columns="28">
+ <image source="ss13-icons/obj/barsigns.png" width="896" height="928"/>
+</tileset>
