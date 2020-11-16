ab218e33dfeeda210248ea6b6809a9cd2e0c0de1
Erin Grasmick <erin@parity.io>
2020-11-16 02:30:35 +0100

    add test bar

---
+++ tilesets/glass_table.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="glass_table" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="../ss13-icons/obj/smooth_structures/glass_table.png" width="224" height="224"/>
+</tileset>
---
+++ tilesets/lighting_object.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="lighting_object" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="../ss13-icons/effects/lighting_object.png" width="96" height="64"/>
+</tileset>
