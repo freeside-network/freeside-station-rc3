95370e4f03af0b41081548230c66e1ec744da43e
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2020-11-16 12:50:54 +0000

    Fix path for freeside graffiti tsx

--- oc-tiles/rc3-freeside-sign.tsx
+++ oc-tiles/rc3-freeside-sign.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="rc3-freeside-sign" tilewidth="32" tileheight="32" tilecount="3" columns="3">
- <image source="org/freeside/rc3-freeside-sign.png" width="96" height="32"/>
+ <image source="rc3-freeside-sign.png" width="96" height="32"/>
 </tileset>
