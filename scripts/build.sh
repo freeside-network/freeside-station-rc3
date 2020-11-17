340ec411573cb12bbd1852ac7c74d464aa9a13b2
Martin Pugh <pugh@s3kr.it>
2020-11-17 02:54:25 +0100

    only sync matching images

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,12 @@ for f in ${GITHUB_WORKSPACE}/*.tmx; do
   xvfb-run "$HOME/tiled" --embed-tilesets --export-map "$f" "${f%.tmx}.json"
 done
 
-rsync -Pav -e "ssh -o 'StrictHostKeyChecking=no' -i $SSH_KEY_PATH" ${GITHUB_WORKSPACE}/*.json ${GITHUB_WORKSPACE}/oc-tiles ${GITHUB_WORKSPACE}/ss13-icons freesidestn@freeside.network:www/
+cat ${GITHUB_WORKSPACE}/*.json | jq -r '.tilesets[].image' | sort -u > images_to_sync
+
+cat images_to_sync
+
+# Sync main files
+rsync -Pav -e "ssh -o 'StrictHostKeyChecking=no' -i $SSH_KEY_PATH" ${GITHUB_WORKSPACE}/*.json freesidestn@freeside.network:www/
+
+# And sync matched resources
+rsync -Pav -e "ssh -o 'StrictHostKeyChecking=no' -i $SSH_KEY_PATH" --files-from=images_to_sync . freesidestn@freeside.network:www/
