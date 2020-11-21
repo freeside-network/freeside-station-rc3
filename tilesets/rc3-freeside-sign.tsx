15db0ccba4a3c9bcdf71b58d853b0713fda4ce4a
your name <you@example.com>
2020-11-21 02:17:08 +0000

    fixing missing tsx

--- tilesets/rc3-freeside-sign.tsx
+++ tilesets/rc3-freeside-sign.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="rc3-freeside-sign" tilewidth="32" tileheight="32" tilecount="3" columns="3">
- <image source="rc3-freeside-sign.png" width="96" height="32"/>
+<tileset version="1.4" tiledversion="1.4.2" name="rc3-freeside-sign" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../oc-tiles/rc3-freeside-sign.png" width="256" height="256"/>
 </tileset>
