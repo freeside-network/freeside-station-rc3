0866372ef306d18e4ea110492647aed660e1293b
your name <you@example.com>
2020-11-22 00:48:06 +0000

    big ol draft

--- tilesets/rc3-freeside-sign.tsx
+++ tilesets/rc3-freeside-sign.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="rc3-freeside-sign" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../oc-tiles/rc3-freeside-sign.png" width="256" height="256"/>
+<tileset version="1.4" tiledversion="1.4.2" name="rc3-freeside-sign" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../oc-tiles/rc3-freeside-sign.png" width="512" height="512"/>
 </tileset>
