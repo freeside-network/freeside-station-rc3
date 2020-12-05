84c959ab736c6522f0a3f24269e3dc3d733351f3
boreq <boreq@0x46.net>
2020-12-06 00:08:05 +0100

    Animate lights

--- tilesets/cccamp2019.tsx
+++ tilesets/cccamp2019.tsx
@@ -1,4 +1,43 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="cccamp2019" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="../oc-tiles/cccamp2019.png" width="512" height="512"/>
+ <tile id="35">
+  <animation>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="99" duration="100"/>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="35" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <animation>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="68" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="100" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="68" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="36" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="37">
+  <animation>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="101" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="37" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
