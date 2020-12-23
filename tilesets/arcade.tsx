1d3bced1e65f0d64e18b59768f1e1e604904c3e9
your name <you@example.com>
2020-12-23 23:37:05 +0000

    arcade machines

---
+++ tilesets/arcade.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="arcade" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../oc-tiles/arcade.png" width="256" height="256"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="400"/>
+   <frame tileid="1" duration="400"/>
+   <frame tileid="2" duration="400"/>
+   <frame tileid="3" duration="400"/>
+   <frame tileid="4" duration="400"/>
+   <frame tileid="5" duration="400"/>
+   <frame tileid="6" duration="400"/>
+   <frame tileid="7" duration="400"/>
+   <frame tileid="16" duration="400"/>
+   <frame tileid="17" duration="400"/>
+   <frame tileid="18" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="600"/>
+   <frame tileid="20" duration="600"/>
+   <frame tileid="21" duration="600"/>
+   <frame tileid="22" duration="600"/>
+   <frame tileid="23" duration="600"/>
+   <frame tileid="32" duration="600"/>
+   <frame tileid="33" duration="600"/>
+   <frame tileid="34" duration="600"/>
+   <frame tileid="35" duration="600"/>
+   <frame tileid="36" duration="600"/>
+   <frame tileid="37" duration="600"/>
+   <frame tileid="38" duration="600"/>
+   <frame tileid="39" duration="600"/>
+  </animation>
+ </tile>
+</tileset>
