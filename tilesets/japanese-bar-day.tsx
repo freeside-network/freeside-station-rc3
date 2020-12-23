32c012bf27da5c803bb06d46454de47594fbba21
boreq <boreq@0x46.net>
2020-12-23 14:47:06 +0100

    Add japanese bar tilesets

---
+++ tilesets/japanese-bar-day.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="japanese-bar-day" tilewidth="32" tileheight="32" tilecount="576" columns="36">
+ <image source="../japanese-bar/japanese-bar-day.png" width="1152" height="512"/>
+</tileset>
---
+++ tilesets/japanese-bar-night.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="japanese-bar-night" tilewidth="32" tileheight="32" tilecount="720" columns="40">
+ <image source="../japanese-bar/japanese-bar-night.png" width="1280" height="576"/>
+</tileset>
