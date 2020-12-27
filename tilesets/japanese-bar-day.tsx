9dae669921285545ed92097f42e5c883d01c204c
your name <you@example.com>
2020-12-27 01:22:34 +0000

    Lots of work

--- tilesets/japanese-bar-day.tsx
+++ tilesets/japanese-bar-day.tsx
@@ -1,4 +1,69 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="japanese-bar-day" tilewidth="32" tileheight="32" tilecount="576" columns="36">
+<tileset version="1.4" tiledversion="1.4.2" name="japanese-bar-day" tilewidth="32" tileheight="32" tilecount="576" columns="36">
  <image source="../japanese-bar/japanese-bar-day.png" width="1152" height="512"/>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="250"/>
+   <frame tileid="52" duration="250"/>
+   <frame tileid="88" duration="250"/>
+   <frame tileid="124" duration="250"/>
+   <frame tileid="160" duration="250"/>
+   <frame tileid="196" duration="250"/>
+   <frame tileid="232" duration="250"/>
+   <frame tileid="268" duration="250"/>
+   <frame tileid="304" duration="250"/>
+   <frame tileid="340" duration="250"/>
+   <frame tileid="376" duration="250"/>
+   <frame tileid="412" duration="250"/>
+   <frame tileid="448" duration="250"/>
+   <frame tileid="484" duration="250"/>
+   <frame tileid="520" duration="250"/>
+   <frame tileid="556" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <animation>
+   <frame tileid="18" duration="250"/>
+   <frame tileid="54" duration="250"/>
+   <frame tileid="90" duration="250"/>
+   <frame tileid="126" duration="250"/>
+   <frame tileid="162" duration="250"/>
+   <frame tileid="198" duration="250"/>
+   <frame tileid="234" duration="250"/>
+   <frame tileid="270" duration="250"/>
+   <frame tileid="306" duration="250"/>
+   <frame tileid="342" duration="250"/>
+   <frame tileid="378" duration="250"/>
+   <frame tileid="414" duration="250"/>
+   <frame tileid="450" duration="250"/>
+   <frame tileid="486" duration="250"/>
+   <frame tileid="522" duration="250"/>
+   <frame tileid="558" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="221">
+  <animation>
+   <frame tileid="221" duration="250"/>
+   <frame tileid="257" duration="250"/>
+   <frame tileid="293" duration="250"/>
+   <frame tileid="329" duration="250"/>
+   <frame tileid="365" duration="250"/>
+   <frame tileid="401" duration="250"/>
+   <frame tileid="437" duration="250"/>
+   <frame tileid="473" duration="250"/>
+   <frame tileid="509" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="361">
+  <animation>
+   <frame tileid="361" duration="800"/>
+   <frame tileid="397" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="362">
+  <animation>
+   <frame tileid="362" duration="800"/>
+   <frame tileid="398" duration="800"/>
+  </animation>
+ </tile>
 </tileset>
---
+++ tilesets/silver_buddha.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="silver_buddha" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="../oc-tiles/silver_buddha.png" width="64" height="96"/>
+</tileset>
