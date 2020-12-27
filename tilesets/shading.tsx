b8fc2feca74c357a33639b361105d72011b0d157
Martin Pugh <pugh@s3kr.it>
2020-12-27 01:34:56 +0100

    don't break the repo, retard

--- tilesets/shading.tsx
+++ tilesets/shading.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="shading" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="oc-tiles/shading.png" width="64" height="64"/>
+ <image source="../oc-tiles/shading.png" width="64" height="64"/>
 </tileset>
