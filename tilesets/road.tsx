6e3f5638f931031cba7122561eae4a531fcfb3d6
Martin Pugh <pugh@s3kr.it>
2020-12-27 04:10:47 +0100

    add a complete mess of a road

---
+++ tilesets/road.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="road" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="../oc-tiles/road.png" width="160" height="160"/>
+</tileset>
