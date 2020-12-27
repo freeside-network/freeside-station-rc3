caed6b1079996e5a6143609dbd47502f9feccb40
Erin Grasmick <erin@parity.io>
2020-12-28 00:06:30 +0100

    add emergency bar animation

--- tilesets/rc3-freeside-sign.tsx
+++ tilesets/rc3-freeside-sign.tsx
@@ -1,4 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="rc3-freeside-sign" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../oc-tiles/rc3-freeside-sign.png" width="512" height="512"/>
+<tileset version="1.4" tiledversion="1.4.3" name="rc3-freeside-sign" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="../oc-tiles/rc3-freeside-sign.png" width="768" height="768"/>
+ <tile id="194">
+  <animation>
+   <frame tileid="194" duration="100"/>
+   <frame tileid="240" duration="100"/>
+   <frame tileid="200" duration="100"/>
+   <frame tileid="224" duration="100"/>
+   <frame tileid="194" duration="100"/>
+   <frame tileid="246" duration="100"/>
+   <frame tileid="194" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="195">
+  <animation>
+   <frame tileid="195" duration="100"/>
+   <frame tileid="241" duration="100"/>
+   <frame tileid="201" duration="100"/>
+   <frame tileid="225" duration="100"/>
+   <frame tileid="195" duration="100"/>
+   <frame tileid="247" duration="100"/>
+   <frame tileid="195" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
