bc8408f28a07e1d5f551c38e13b3b0bb06208cfa
your name <you@example.com>
2020-12-27 20:19:21 +0000

    update arcade + trash

--- tilesets/allcreatureswelcome_neon.tsx
+++ tilesets/allcreatureswelcome_neon.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="allcreatureswelcome_neon" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+<tileset version="1.4" tiledversion="1.4.2" name="allcreatureswelcome_neon" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="../oc-tiles/allcreatureswelcome_neon.png" width="64" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="800"/>
+   <frame tileid="2" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="800"/>
+   <frame tileid="3" duration="800"/>
+  </animation>
+ </tile>
 </tileset>
---
+++ tilesets/trash3x3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="trash3x3" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../oc-tiles/trash3x3.png" width="96" height="96"/>
+</tileset>
