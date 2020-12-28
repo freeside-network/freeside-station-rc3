41974ec693f8c55ff16816faad7bbeb12abca407
Martin Pugh <pugh@s3kr.it>
2020-12-28 23:22:04 +0100

    add s3krits

--- tilesets/pkmn.tsx
+++ tilesets/pkmn.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="pkmn" tilewidth="16" tileheight="16" tilecount="1080" columns="36">
- <image source="../oc-tiles/pkmn.png" width="576" height="480"/>
- <tile id="3">
+<tileset version="1.4" tiledversion="1.4.3" name="pkmn" tilewidth="32" tileheight="32" tilecount="1080" columns="36">
+ <image source="../oc-tiles/pkmn.png" width="1152" height="960"/>
+ <tile id="1">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
