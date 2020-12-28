81fb86fe47fc686e994d7d6503e7d90235b448af
Martin Pugh <pugh@s3kr.it>
2020-12-28 23:07:50 +0100

    add pkmn

---
+++ tilesets/pkmn.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pkmn" tilewidth="16" tileheight="16" tilecount="1080" columns="36">
+ <image source="../oc-tiles/pkmn.png" width="576" height="480"/>
+ <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
