d66987e429666c2dde08b46f7028d8c877b82e27
Martin Pugh <pugh@s3kr.it>
2020-12-28 22:11:27 +0100

    add hackers

---
+++ tilesets/hackers_screen.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="hackers_screen" tilewidth="32" tileheight="32" tilecount="119" columns="17">
+ <image source="../oc-tiles/hackers_screen.png" width="544" height="224"/>
+</tileset>
