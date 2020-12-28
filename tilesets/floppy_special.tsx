18197d12c6f200af592e816d7f0add53ed69ac64
Erin Grasmick <erin@parity.io>
2020-12-28 19:02:14 +0100

    secrets

---
+++ tilesets/floppy_special.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="floppy_special" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../oc-tiles/floppy_special.png" width="96" height="96"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
