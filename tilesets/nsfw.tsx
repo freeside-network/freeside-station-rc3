4b2f04d05976d44b09ebf01b6ffed32dac724f2d
Erin Grasmick <erin@parity.io>
2020-12-31 00:53:12 +0100

    fixes

--- tilesets/nsfw.tsx
+++ tilesets/nsfw.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Mattress" tilewidth="32" tileheight="32" tilecount="72" columns="8">
- <image source="../../../freeside/rc3/Mattress.png" width="256" height="288"/>
+ <image source="../oc-tiles/nsfw.png" width="256" height="288"/>
 </tileset>
