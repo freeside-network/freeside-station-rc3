23c1557c5bc1747a5c982c0e2fb852378ac98d18
boreq <boreq@0x46.net>
2020-12-31 01:31:46 +0100

    Add a fire

---
+++ tilesets/fire.tsx
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fire" tilewidth="32" tileheight="32" tilecount="240" columns="20">
+ <image source="../oc-tiles/fire.png" width="640" height="384"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="46" duration="100"/>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="56" duration="100"/>
+   <frame tileid="58" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="47" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="55" duration="100"/>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="59" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="62" duration="100"/>
+   <frame tileid="64" duration="100"/>
+   <frame tileid="66" duration="100"/>
+   <frame tileid="68" duration="100"/>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="72" duration="100"/>
+   <frame tileid="74" duration="100"/>
+   <frame tileid="76" duration="100"/>
+   <frame tileid="78" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="65" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="71" duration="100"/>
+   <frame tileid="73" duration="100"/>
+   <frame tileid="75" duration="100"/>
+   <frame tileid="77" duration="100"/>
+   <frame tileid="79" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
