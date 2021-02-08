94281608d4ef28e7b3f4e44c6a799d73b8855a7b
Erin Grasmick <erin@parity.io>
2021-02-08 12:43:30 +0100

    remove end-of-event modifications (reverted to 4b2f04d05976d44b09ebf01b6ffed32dac724f2d)

--- tilesets/farewell.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="farewell" tilewidth="32" tileheight="32" tilecount="6" columns="6">
- <image source="../oc-tiles/farewell.png" width="192" height="32"/>
-</tileset>
