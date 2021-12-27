ef0b636deb1f4fa5e6db41de07ccd16d73b7681e
lovelaced <erin@parity.io>
2021-12-27 15:36:24 +0100

    add licenses

--- tilesets/japanese-bar-day.tsx
+++ tilesets/japanese-bar-day.tsx
@@ -1,5 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="japanese-bar-day" tilewidth="32" tileheight="32" tilecount="576" columns="36">
+ <properties>
+  <property name="tilesetCopyright">You can use the Licensed Asset to create Derivative Works; and
+
+(a) use the Licensed Asset and any Derivative Works as part of both Non-Monetized Media Products and Monetized Media Products, with no restriction on the number of projects the Licensed Asset may be used in. In either case, the Licensed Assets can be used in Media Products that are either:
+
+(b) used for the Purchaser’s own personal use; and/or
+
+(c) used for the Purchaser’s commercial use in which case it may be distributed, sold and supplied by the Purchaser for any fee that the Purchaser may determine.
+
+A Licence does not allow the Purchaser to:
+
+(a1) Use the Licensed Asset or Derivative Works in a logo, trademark or service mark;
+
+(b1) Use, sell, share, transfer, give away, sublicense or redistribute the Licensed Asset or Derivate Works other than as part of the relevant Media Product; or
+
+(c1) Allow the user of the Media Product to extract the Licensed Asset or Derivative Works and use them outside of the relevant Media Product.</property>
+ </properties>
  <image source="../japanese-bar/japanese-bar-day.png" width="1152" height="512"/>
  <tile id="16">
   <animation>
